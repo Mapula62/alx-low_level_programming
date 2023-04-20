719f018cb7d855f7554f96832283a16eb67db7eb
Mapula62 <mmatabane62@gmail.com>
2023-04-20 08:59:32 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A c program that prints to the line standard error
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\", 59);
+	return (1);
+}
