957af2c455d016d7eba77086a8fab94d672c0577
Mapula62 <mmatabane62@gmail.com>
2023-04-25 14:26:00 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in lowercase
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
