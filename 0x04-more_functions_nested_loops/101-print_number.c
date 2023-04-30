14283a39cf4e54101f888764f8e97c7d1d34ceed
Mapula62 <mmatabane62@gmail.com>
2023-04-29 19:44:52 -0700

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,16 +6,14 @@
  */
 void print_number(int n)
 {
-unsinged int i = n;
 if (n < 0)
 {
 _putchar(45);
-i = -i;
+n = -0;
 }
-if (i / 10)
+if (n / 10)
 {
-print_number(i / 10);
+print_number(n / 10);
 }
-_putchar(i % 10 + '0');
-return (0);
+_putchar(n % 10 + '0');
 }
