3ba82be4cb88bb642d166c525c4f34d3a49e474d
Mapula62 <mmatabane62@gmail.com>
2023-05-05 06:20:06 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,14 +4,14 @@
  * @str: String to be capitalized
  * Return: String pointer to be changed
  */
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
 int string_count = 0;
 while (str[string_count])
 {
-while (!([string_count] >= 'a' && str[string_count] <= 'z'))
+while (!(str[string_count] >= 'a' && str[string_count] <= 'z'))
 string_count++;
-if (str[string_count - 1] == ' ' || 
+if (str[string_count - 1] == ' ' ||
 str[string_count - 1] == '\t' ||
 str[string_count - 1] == '\n' ||
 str[string_count - 1] == ',' ||
