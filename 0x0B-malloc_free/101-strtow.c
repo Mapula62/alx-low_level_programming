5c51674081db31c082c8729dbf600eb3c93b251d
Mapula62 <mmatabane62@gmail.com>
2023-05-17 08:37:50 -0700

    task6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,19 +7,20 @@
 */
 int count_word(char *s)
 {
-int a, b, flag;
-b = 0;
+int flag, c, w;
 flag = 0;
-for (a = 0; s[a] != '\0'; a++)
+w = 0;
+for (c = 0; s[c] != '\0'; c++)
 {
-if (s[a] == ' ')
+if (s[c] == ' ')
 flag = 0;
 else if (flag == 0)
 {
 flag = 1;
-b++;
+w++;
 }
-return (b);
+}
+return (w);
 }
 /**
  * *strtow - string to be splits into words
@@ -29,36 +30,35 @@ return (b);
 char **strtow(char *str)
 {
 char **matrix, *tmp;
-int a, b = 0, len = 0, words, l = 0, start, end;
-while (*(str + len))
+int i, k = 0, len = 0, words, c = 0, start, end;
+while (*(str + len)) 
 len++;
 words = count_word(str);
 if (words == 0)
-return (NULL);
+return (NULL); 
 matrix = (char **) malloc(sizeof(char *) * (words + 1));
 if (matrix == NULL)
-return (NULL);
-for (a = 0; a <= len; a++)
+return (NULL); 
+for (i = 0; i <= len; i++)
 {
-if (str[a] == ' ' || str[a] == '\0')
+if (str[i] == ' ' || str[i] == '\0')
 {
-if (l)
+if (c)
 {
-end = a;
-tmp = (char *) malloc(sizeof(char) * (l + 1));
+end = i;tmp = (char *) malloc(sizeof(char) * (c + 1));
 if (tmp == NULL)
-return (NULL);
+return (NULL); 
 while (start < end)
 *tmp++ = str[start++];
-*tmp = '\0';
-matrix[b] = tmp - l;
-b++;
-l = 0;
+*tmp = '\0'; 
+matrix[k] = tmp - c;
+k++;  
+c = 0;
 }
 }
-else if (l++ == 0)
-start = a;
+else if (c++ == 0)
+start = i;
 }
-matrix[b] = NULL;
+matrix[k] = NULL;
 return (matrix);
 }
