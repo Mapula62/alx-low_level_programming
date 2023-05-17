f53375f7f0b10fa033871c9a7ee6eef52490d576
Mapula62 <mmatabane62@gmail.com>
2023-05-17 08:50:30 -0700

    fixed errors

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-* strtow - a function that splits a string into words
+* count_word - splits a string into words
 * @s: input string
 * Return: num of words
 */
@@ -31,28 +31,28 @@ char **strtow(char *str)
 {
 char **matrix, *tmp;
 int i, k = 0, len = 0, words, c = 0, start, end;
-while (*(str + len)) 
+while (*(str + len))
 len++;
 words = count_word(str);
 if (words == 0)
-return (NULL); 
+return (NULL);
 matrix = (char **) malloc(sizeof(char *) * (words + 1));
 if (matrix == NULL)
-return (NULL); 
+return (NULL);
 for (i = 0; i <= len; i++)
 {
 if (str[i] == ' ' || str[i] == '\0')
 {
 if (c)
 {
-end = i;tmp = (char *) malloc(sizeof(char) * (c + 1));
+end = i; tmp = (char *) malloc(sizeof(char) * (c + 1));
 if (tmp == NULL)
-return (NULL); 
+return (NULL);
 while (start < end)
 *tmp++ = str[start++];
-*tmp = '\0'; 
+*tmp = '\0';
 matrix[k] = tmp - c;
-k++;  
+k++;
 c = 0;
 }
 }
