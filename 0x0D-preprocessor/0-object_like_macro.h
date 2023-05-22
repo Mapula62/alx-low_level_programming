4ff5a63e30592ea2cf3b0a09e79aa11e5a61c601
Mapula62 <mmatabane62@gmail.com>
2023-05-22 08:16:58 -0700

    all tasks completed

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LLIKE_MACRO_H
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - name of the file to be printed
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
