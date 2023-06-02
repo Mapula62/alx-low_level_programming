b4ee126dfea8b1f33e4540000c50855e488858a7
Mapula62 <mmatabane62@gmail.com>
2023-06-02 15:00:58 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void _constructor(void)__attribute__((constructor));
+/**
+ * _constructor - prints a function that executes before main
+ * Return: nothing
+ */
+void _constructor(void)
+{
+printf("You're best! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
