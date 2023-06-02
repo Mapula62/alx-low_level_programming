7675ed66632673cb2cbe78e7254478beb50054d5
Mapula62 <mmatabane62@gmail.com>
2023-06-02 15:40:17 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@ void _constructor(void)__attribute__((constructor));
  */
 void _constructor(void)
 {
-printf("You're best! and yet, you must allow,\n");
-printf("I bore my house upon my back!\n");
+printf("You're best! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
 }
