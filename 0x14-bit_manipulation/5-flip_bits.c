373b47f9522f3158f58c5cd1814ccbb1252cf07d
Mapula62 <mmatabane62@gmail.com>
2023-06-08 06:44:32 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * flip_bits - returns the number of bits needed to be flippled
+ * @n: input first number
+ * @m: input second number
+ * Return: number of bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned int num = 0;
+unsigned long int sum = n ^ m;
+while (sum)
+{
+if (sum & 1ul)
+num++;
+sum = sum >> 1;
+}
+return (num);
+}
