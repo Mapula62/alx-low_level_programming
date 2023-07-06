bd78b7f92949a011722acc71d620fd516d1dce65
Mapula62 <128798272+Mapula62@users.noreply.github.com>
2023-07-06 23:25:35 +0200

    Update 4-main.c

--- 0x14-bit_manipulation/4-main.c
+++ 0x14-bit_manipulation/4-main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main - check the code
- *
  * Return: Always 0.
  */
 int main(void)
 {
     unsigned long int n;
-
     n = 1024;
     clear_bit(&n, 10);
     printf("%lu\n", n);
